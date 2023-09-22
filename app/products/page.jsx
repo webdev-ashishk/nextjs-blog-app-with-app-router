dc4694c14c66194ee6a5a2ae845989e26775d757
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-22 12:14:07 +0530

    issue is make image center in div

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -32,10 +32,15 @@ export default async function Products() {
                     width={300}
                     height={300}
                   ></Image>
-                  <h1>{product?.title}</h1>
+                  <h1 className="font-bold text-2xl m-3">{product?.title}</h1>
                   <ul>
                     <li>{product.description}</li>
-                    <li>Price - {product.price} $</li>
+                    <span className="text-yellow-400">
+                      rating-{product.rating}
+                    </span>
+                    <li className="text-2xl font-bold text-purple-500">
+                      Price - {product.price} $
+                    </li>
                   </ul>
                 </div>
               </>
